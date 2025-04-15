c690e13a9ac5935f544a0242d728778f4e7468e0
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-15 14:53:40 +0300

    Add 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,10 @@ import Footer from './components/footer/Footer';
 
 import Navbar from './components/navigation/navbar-lrg/Navbar';
 import NavbarSm from './components/navigation/navbar-sm/NavbarSm';
-import AuthProfile from './components/page/aouth/authProfile/AuthProfile';
+import AuthProfile from './components/page/auth/authProfile/AuthProfile';
 import HomePage from './components/page/HomePage';
 import { UserProvider } from './contexts/UserContext';
+import PageNotFound from './components/page/404/PageNotFound';
 
 export default function App() {
     return (
@@ -16,10 +17,11 @@ export default function App() {
                 <NavbarSm />
 
                 <Routes>
-                    <Route index element={<HomePage />}/>
-                    <Route path='/auth/:id/profile'element={<AuthProfile />} />                    
+                    <Route index element={<HomePage />} />
+                    <Route path='/auth/:id/profile' element={<AuthProfile />} />
+                    <Route path='*' element={<PageNotFound />} />
                 </Routes>
-                
+
                 <Footer />
             </div>
         </UserProvider>
---
+++ client/src/components/page/404/PageNotFound.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router";
+
+export default function PageNotFound() {
+    return (
+        <div className="w3-display-container w3-light-grey w3-padding-64" style={{ height: "100vh" }}>
+            <div className="w3-display-middle w3-center">
+                <img
+                    src="https://cdn-icons-png.flaticon.com/512/4712/4712109.png"
+                    alt="Robot confused"
+                    style={{ width: "200px", maxWidth: "90%" }}
+                    className="w3-animate-top"
+                />
+                <h1 className="w3-xxxlarge w3-text-grey w3-animate-opacity">404</h1>
+                <p className="w3-large w3-text-dark-grey w3-animate-opacity">
+                    Oops! The page you're looking for doesn't exist.
+                </p>
+                <Link to="/" className="w3-button w3-blue w3-round-large w3-margin-top w3-hover-shadow">
+                    Go Home
+                </Link>
+            </div>
+        </div>
+    );
+};
